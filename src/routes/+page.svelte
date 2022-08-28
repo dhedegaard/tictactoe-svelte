2221f25b98222e6755cf51f42ae64a658dfc96f8
Dennis Hedegaard <dennis@dhedegaard.dk>
2022-08-28 10:11:28 +0200

    initial skeleton

---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<div class="container">
+	<h1>Tic tac toe</h1>
+	<hr />
+	<p>
+		An implementation of tic tac toe in it's simplest form, for simply learning and understanding
+		how svelte works.
+	</p>
+</div>
+
+<style>
+	:global(body) {
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Helvetica, Arial, sans-serif,
+			'Apple Color Emoji', 'Segoe UI Emoji';
+		margin: 0;
+		padding: 0;
+	}
+
+	.container {
+		max-width: 800px;
+		margin: 16px auto 0;
+	}
+
+	h1 {
+		text-align: center;
+	}
+</style>
